1a1fbff30747536f1719d6bc852648277761e287
AndreyDodonov <Nordhummer@gmail.com>
2019-07-01 08:15:45 +0300

    lesson2

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ let money,
     addExpenses,
     deposit,
     mission,
-    period ;
+    period,
+    budgetDay ;
 
-alert("variables created");    
-console.log("no errors");
+money = 50000;
+income = 'freelance';
+addExpenses = 'courses, travels, clothes';
+deposit = true;
+mission = 2000000;
+period = 5;
+budgetDay = money / 30;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(income.length);
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase(addExpenses));
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
+
