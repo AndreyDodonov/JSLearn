70d7f7f11c35c91eb856eccbc4ebd1e4175ab0cc
AndreyDodonov <Nordhummer@gmail.com>
2019-07-15 12:37:46 +0300

    rewrite cycle for expensesMonth

--- script.js
+++ script.js
@@ -19,76 +19,67 @@ let appData = {
     period: 3,
     budget: money,
     budgetDay: function () {
-        this.budgetDay = (money / 30);
+        this.budgetDay = (this.budget / 30);
         console.log('дневной бюджет: ' + this.budgetDay);
     },
     expensesMonth: function () {
         let sum = 0;
         for (let i = 0; i < 2; i++) {
             if (i === 0) {
-                prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Расход1');
+               let key1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Расход1');
             } else if (i === 1) {
-                prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Расход2');
+               let key1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Расход2');
             }
             do {
-                sum += +prompt('Во сколько это обойдётся?', 10000);
+                let value = +prompt('Во сколько это обойдётся?', 10000);
+                appData.expenses[key1] = value;
+
             } while (isNaN(sum) || sum == '' || sum == null);
         }
         return sum;
     },
+    accumulateMonth: this.expensesMonth(),
     budgetMonth: function () {
-        return (this.budget - expensesMonth);
+        return (this.budget - this.accumulateMonth);
     },
     asking: function () {
-        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 
-        'расх1, расх2, расх3');
+        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+            'расх1, расх2, расх3');
         appData.addExpenses = addExpenses.toLowerCase().split(',');
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
     },
-
+    targetPeriod: function () {
+        return ((this.budget * this.period) - (this.accumulateMonth * this.period));
+    },
+    budgetPeriod: function () {
+        return (this.budget * this.period);
+    },
+    expensePeriod: function () {
+        return (this.accumulateMonth * this.period);
+    },
+    incomePeriod: function () {
+        return (this.budgetPeriod() - this.expensePeriod());
+    },
+    getStatusIncome: function () {
+        if (this.budgetDay >= 800) {
+            return ('Высокий уровень дхода');
+        } else if (this.budgetDay >= 300) {
+            return ('Средний уровень дохода');
+        } else if (this.budgetDay < 0) {
+            return ('Что-то пошло не так');
+        } else if (this.budgetDay < 300) {
+            return ('Низкий уровень дохода');
+        }
+    }
 };
-
 
 appData.asking();
 appData.budgetDay();
-let expensesMonth = appData.expensesMonth(),
-    accumulateMonth = appData.budgetMonth();
-
-function getStatusIncome() {
-    if (appData.budgetDay >= 800) {
-        return ('Высокий уровень дхода');
-    } else if (appData.budgetDay >= 300) {
-        return ('Средний уровень дохода');
-    } else if (appData.budgetDay < 0) {
-        return ('Что-то пошло не так');
-    } else if (appData.budgetDay < 300) {
-        return ('Низкий уровень дохода');
-    }
-}
-
-let expensePeriod = function () {
-    return (expensesMonth * appData.period);
-};
-
-
-let budgetPeriod = function () {
-    return (money * appData.period);
-};
-
-let incomePeiod = function () {
-    return (budgetPeriod() - expensePeriod());
-};
-
-let targetPeriod = function () {
-    return (appData.mission / accumulateMonth);
-};
-
-
 
 /* output */
 
-console.log('Расходы за месяц: ' + expensesMonth);
-console.log('Уровень дохода: ', getStatusIncome());
-console.log((targetPeriod() > 0) ? 'Срок достижения цели: ' + Math.floor(targetPeriod()) + ' месяцев' :
+console.log('Расходы за месяц: ' + appData.expensesMonth);
+console.log('Уровень дохода: ', appData.getStatusIncome());
+console.log((appData.targetPeriod > 0) ? 'Срок достижения цели: ' + Math.floor(appData.targetPeriod) + ' месяцев' :
     'цель не будет достигнута ((');
-console.log('Накопления за период ' + appData.period + ' месяцев: ' + (accumulateMonth * appData.period));
+console.log('Накопления за период ' + appData.period + ' месяцев: ' + (appData.accumulateMonth * appData.period));
