87e88d69cb216447dd5f83be02742096cfaab0ef
AndreyDodonov <Nordhummer@gmail.com>
2019-07-16 08:09:39 +0300

    add more validations, add getInfoDeposit in asking

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let appData = {
     period: 3,
     budget: money,
     budgetDay: function () {
-       return (this.getBudget() / 30);
+        return (this.getBudget() / 30);
     },
     expensesMonth: 0,
     getExpensesMonth: function () {
@@ -29,11 +29,11 @@ let appData = {
         let key;
         for (let i = 0; i < 2; i++) {
             do {
-            key = prompt('Какие обязательные ежемесячные расходы у вас есть?','№'+ (i+1) + ' Расход' );
-            } while (key == '' || key == null );
+                key = prompt('Какие обязательные ежемесячные расходы у вас есть?', '№' + (i + 1) + ' Расход');
+            } while (key == '' || key == null);
             do {
-             value = +prompt('Во сколько это обойдётся?', 10000);
-            } while (isNaN(value) || value == '' || value == null );
+                value = +prompt('Во сколько это обойдётся?', 10000);
+            } while (isNaN(value) || value == '' || value == null);
             appData.expenses[key] = value;
             this.expensesMonth += value;
         }
@@ -43,10 +43,17 @@ let appData = {
     },
     asking: function () {
         if (confirm('Есть ли у вас дополнительный заработок?')) {
-            let itemIncome = prompt('Какой у вас есть дополнительный заработок?', 'Таксую');
-            let cashIncome = +prompt('Сколько зарабатываете этим?', 10000);
+            let itemIncome,
+                cashIncome;
+            do {
+                itemIncome = prompt('Какой у вас есть дополнительный заработок?', 'Таксую');
+            } while (itemIncome == '' || itemIncome == null);
+            do {
+                cashIncome = +prompt('Сколько зарабатываете этим?', 10000);
+            } while (isNaN(cashIncome) || cashIncome == '' || cashIncome == null);
             this.income[itemIncome] = cashIncome;
-        } 
+            this.getBudget += cashIncome;
+        }
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
             'расх1, расх2, расх3');
         this.addExpenses = addExpenses.toLowerCase().split(',');
@@ -78,19 +85,18 @@ let appData = {
         }
     },
     getInfoDeposit: function () {
-        if(this.deposit) {
+        if (this.deposit) {
             do {
-            this.percentDeposit = +prompt('Какой годовой процент на депозите?', 10);
-            } while (isNaN(this.percentDeposit) || this.percentDeposit == '' || this.percentDeposit == null );
+                this.percentDeposit = +prompt('Какой годовой процент на депозите?', 10);
+            } while (isNaN(this.percentDeposit) || this.percentDeposit == '' || this.percentDeposit == null);
             do {
-            this.moneyDeposit = +prompt('Какая сумма на депозите?', 10000);
-            } while (isNaN(this.moneyDeposit) || this.moneyDeposit == '' || this.moneyDeposit == null );
+                this.moneyDeposit = +prompt('Какая сумма на депозите?', 10000);
+            } while (isNaN(this.moneyDeposit) || this.moneyDeposit == '' || this.moneyDeposit == null);
         }
     },
     calcSavedMoney: function () {
         return (this.getBudget() * this.period);
     }
-
 };
 
 appData.asking();
@@ -106,8 +112,8 @@ function appDataShow() {
 
 console.log('Расходы за месяц: ' + appData.expensesMonth);
 console.log('Уровень дохода: ', appData.getStatusIncome());
-console.log((appData.getTargetMonth() > 0) ? 'Срок достижения цели: ' + Math.ceil(appData.getTargetMonth()) + ' месяцев' :
-    'цель не будет достигнута ((');
+console.log((appData.getTargetMonth() > 0) ? 'Срок достижения цели: ' + Math.ceil(appData.getTargetMonth()) +
+    ' месяцев' : 'цель не будет достигнута ((');
 console.log('Накопления за период ' + appData.period + ' месяцев: ' + appData.incomePeriod());
 console.log('=================================');
 appDataShow();
